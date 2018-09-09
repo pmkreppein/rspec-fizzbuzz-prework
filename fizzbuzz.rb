a3c58655ccc87f1397ca32fa17ad988f1d4465de
Peter Kreppein <pmkreppein@gmail.com>
2018-09-09 23:34:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-    next
   elsif int % 5 == 0
     return "Buzz"
-    next
   elsif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-    next
   else return nil
   end
 end
